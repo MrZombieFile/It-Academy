f2f1c307b07e9d2559183d57f8cba83e9904902c
MrZombieFile <103641610+MrZombieFile@users.noreply.github.com>
2022-05-26 16:27:19 +0200

    Delete Tasca2Exceptions/Nivell1 directory

--- Tasca2Exceptions/Nivell1/Shop2.java
+++
@@ -1,7 +0,0 @@
-package Tasca2Exceptions.Nivell1;
-
-public class Shop2 {
-    public void printHello2(){
-        System.out.println("World Hello!");
-    }
-}
--- Tasca2Exceptions/Nivell1/ShoppingCart/Exceptions/VendaBuidaException.java
+++
@@ -1,9 +0,0 @@
-package Tasca2Exceptions.Nivell1.ShoppingCart.Exceptions;
-
-public class VendaBuidaException extends Exception {
-    public VendaBuidaException(String message){
-
-        super(message);
-
-    }
-}
